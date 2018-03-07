a9ab7c211cf024e7acf03ec44d4707682a3380fd
AnyaKilpatrick <hannakilpatrick@gmail.com>
2018-03-07 15:35:17 -0500

    modified code for buttons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var text
 var numRecords
 var startYear
 var endYear
-var ourUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8e63e8b39d0745e39ce4503d649aaa7b"
+
 
 
 // 'q': "lincoln",
@@ -11,26 +11,37 @@ var ourUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8
 // 'end_date': "18650410"
 // });
 
-$(".btn").on("click", function(){
+$("#submitButton").on("click", function(){
+    var ourUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8e63e8b39d0745e39ce4503d649aaa7b";
     text = $("#Search").val().trim();
     numRecords = $("#numRecords").val().trim();
-    startYear = $("#startYear").val().trim()
-    endYear = $("#endYear").val().trim();
+    startYear = $("#startYear").val().trim() + "0101";
+    endYear = $("#endYear").val().trim() + "1230";
     // var text = "1";
     // numRecords = "2";
     // startYear = "3";
     // endYear = "4";
     // ourURL = ourUrl + "&q=" + text + "&fq=" + numRecords + "&begin_date=" + startYear + "&end_date=" + endYear;
-    ourUrl += `&q=${text}&fq=${numRecords}&begin_date=${startYear}&end_date=${endYear}`
+    // ourUrl += `&q=${text}&fq=${numRecords}&begin_date=${startYear}&end_date=${endYear}`
+    ourUrl += `&q=${text}&begin_date=${startYear}&end_date=${endYear}`;
     console.log(ourUrl);
 
     $.ajax({
         url: ourUrl,
         method: "Get",
     }).then(function(response){
-        console.log(response)
+        console.log(response);
+        for (var i=0; i<numRecords; i++){
+            $("#articles").append(`
+                <h2><a href="${response.response.docs[i].web_url}" target="_blank">${response.response.docs[i].headline.main}</a></h2>
+                <p>${response.response.docs[i].abstract}</p>
+            `)
+        }
     })
 
 })
+$("#resetButton").on("click", function () {
+    location.reload();
+});     
 
 // url: ourUrl + "&q=" + text + "&fq=" + numRecords + "&begin_date=" + startYear + "&end_date=" + endYear
